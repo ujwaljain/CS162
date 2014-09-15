144e098b408b131932402bc1f0300595db02155b
ujjwalj <ujjwaljain03@gmail.com>
2014-09-14 23:08:36 -0700

    OahuMolokai task

--- nachos/ag/BoatGrader.java
+++ nachos/ag/BoatGrader.java
@@ -1,6 +1,10 @@
 package nachos.ag;
 
-public class BoatGrader {
+import nachos.threads.Boat;
+import nachos.threads.KThread;
+import nachos.machine.Lib;
+
+public class BoatGrader extends BasicTestGrader {
 
     /**
      * BoatGrader consists of functions to be called to show that
@@ -8,13 +12,46 @@ public class BoatGrader {
      * prints messages to standard out, so that you can watch it.
      * You cannot submit this file, as we will be using our own
      * version of it during grading.
-
+     *
      * Note that this file includes all possible variants of how
      * someone can get from one island to another. Inclusion in
      * this class does not imply that any of the indicated actions
      * are a good idea or even allowed.
      */
-	
+	private int adultsOahu, childrenOahu;
+    private int adultsMolokai, childrenMolokai;
+
+    void run() {
+        final int adults = getIntegerArgument("adults");
+        final int children = getIntegerArgument("children");
+
+        Lib.assertTrue(adults >= 0 && children >= 1, 
+            "number can't be negative");
+        this.startTest(adults, children);
+        checkAllCrossed(adults, children);
+        done();
+    }
+
+    private void startTest(int adults, int children) {
+        this.adultsOahu = adults;
+        this.childrenOahu = children;
+        this.adultsMolokai = this.childrenMolokai = 0;
+
+        Boat.begin(adults, children, this);
+    }
+
+    private void checkAllCrossed(int adults, int children) {
+        Lib.assertTrue(adultsOahu == 0, adultsOahu +
+            " adults are still in Oahu");
+        Lib.assertTrue(childrenOahu == 0, childrenOahu +
+            " children are still in Oahu");
+
+        Lib.assertTrue(adultsMolokai == adults, adultsMolokai +
+            " adults reached Molokai");
+        Lib.assertTrue(childrenMolokai == children, childrenMolokai +
+            " children reached Molokai");
+    }
+
 	//NEW ADDITION FOR 2014
 	//MUST BE CALLED AT THE START OF CHILDITINERARY!
 	public void initializeChild(){
@@ -30,49 +67,73 @@ public class BoatGrader {
     /* ChildRowToMolokai should be called when a child pilots the boat
        from Oahu to Molokai */
     public void ChildRowToMolokai() {
-	System.out.println("**Child rowing to Molokai.");
+        Lib.assertTrue(childrenOahu > 0, "no children in Oahu.");
+        childrenOahu--;
+        childrenMolokai++;
+        System.out.println("**" + KThread.currentThread().getName() + 
+            " rowing to Molokai.");
     }
 
     /* ChildRowToOahu should be called when a child pilots the boat
        from Molokai to Oahu*/
     public void ChildRowToOahu() {
-	System.out.println("**Child rowing to Oahu.");
+        Lib.assertTrue(childrenMolokai > 0, "no children in Molokai");
+        childrenOahu++;
+        childrenMolokai--;
+        System.out.println("**" + KThread.currentThread().getName() + 
+            " rowing to Oahu.");
     }
 
     /* ChildRideToMolokai should be called when a child not piloting
        the boat disembarks on Molokai */
     public void ChildRideToMolokai() {
-	System.out.println("**Child arrived on Molokai as a passenger.");
+        Lib.assertTrue(childrenOahu > 0, "no children in Oahu.");
+        childrenOahu--;
+        childrenMolokai++;
+	   System.out.println("**" + KThread.currentThread().getName() + 
+            " arrived on Molokai as a passenger.");
     }
 
     /* ChildRideToOahu should be called when a child not piloting
        the boat disembarks on Oahu */
     public void ChildRideToOahu() {
-	System.out.println("**Child arrived on Oahu as a passenger.");
+        Lib.assertTrue(childrenMolokai > 0, "no children in Molokai");
+        childrenOahu++;
+        childrenMolokai--;
+        System.out.println("**" + KThread.currentThread().getName() + 
+            " arrived on Oahu as a passenger.");
     }
 
     /* AdultRowToMolokai should be called when a adult pilots the boat
        from Oahu to Molokai */
     public void AdultRowToMolokai() {
-	System.out.println("**Adult rowing to Molokai.");
+        Lib.assertTrue(adultsOahu > 0, "no adults in Oahu");
+        adultsOahu--;
+        adultsMolokai++;
+        System.out.println("**Adult rowing to Molokai.");
     }
 
     /* AdultRowToOahu should be called when a adult pilots the boat
        from Molokai to Oahu */
     public void AdultRowToOahu() {
-	System.out.println("**Adult rowing to Oahu.");
+        Lib.assertTrue(adultsMolokai > 0, "no adults in Molokai");
+        adultsMolokai--;
+        adultsOahu++;
+        System.out.println("**Adult rowing to Oahu.");
     }
 
     /* AdultRideToMolokai should be called when an adult not piloting
        the boat disembarks on Molokai */
     public void AdultRideToMolokai() {
-	System.out.println("**Adult arrived on Molokai as a passenger.");
+        Lib.assertNotReached("Invalid Operation: AdultRideToMolokai");
+        System.out.println("**Adult arrived on Molokai as a passenger.");
     }
 
     /* AdultRideToOahu should be called when an adult not piloting
        the boat disembarks on Oahu */
     public void AdultRideToOahu() {
-	System.out.println("**Adult arrived on Oahu as a passenger.");
+        Lib.assertNotReached("Invalid Operation: AdultRideToOahu");
+        System.out.println("**Adult arrived on Oahu as a passenger.");
     }
 }
 
--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -1,76 +1,189 @@
 package nachos.threads;
 import nachos.ag.BoatGrader;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Boat
 {
     static BoatGrader bg;
+	static Condition readyToLeaveOahu, readyToLeaveMolokai;
+	static Condition boatRiders;
+
+	static Lock boatLock;
+	static Lock lock;
+	static int childrenOnOahu, childrenOnMolokai;
+	static int adultsOnOahu, adultsOnMolokai;
+	static boolean lastPersonLeft = false;
+	static int boatIsInUse = 0;		// 0 not used, 1 half used, 2 full used.
+	static int boatEmpty = 0, boatHalfFull = 1, boatFull = 2; 
+	static int boatLocation = 0;	// Oahu
     
     public static void selfTest()
     {
-	BoatGrader b = new BoatGrader();
+		BoatGrader b = new BoatGrader();
 	
-	System.out.println("\n ***Testing Boats with only 2 children***");
-	begin(0, 2, b);
+		System.out.println("\n ***Testing Boats with only 2 children***");
+		begin(0, 2, b);
 
-//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-//  	begin(1, 2, b);
+		//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
+		//  begin(1, 2, b);
 
-//  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
-//  	begin(3, 3, b);
+		//  System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
+		//  begin(3, 3, b);
     }
 
     public static void begin( int adults, int children, BoatGrader b )
     {
-	// Store the externally generated autograder in a class
-	// variable to be accessible by children.
-	bg = b;
+		// Store the externally generated autograder in a class
+		// variable to be accessible by children.
+		bg = b;
+		Set<KThread> set = new HashSet<KThread>();
 
-	// Instantiate global variables here
-	
-	// Create threads here. See section 3.4 of the Nachos for Java
-	// Walkthrough linked from the projects page.
+		// Instantiate global variables here
+		boatLock = new Lock();
+		lock = new Lock();
+		readyToLeaveMolokai = new Condition(lock);
+		readyToLeaveOahu = new Condition(lock);
+		boatRiders = new Condition(boatLock);
 
-	Runnable r = new Runnable() {
-	    public void run() {
-                SampleItinerary();
-            }
-        };
-        KThread t = new KThread(r);
-        t.setName("Sample Boat Thread");
-        t.fork();
+		// Create threads here.
+		for (int i = 0; i < adults; i++) {
+			KThread adultThread = new KThread(new Runnable() {
+				public void run() {
+					AdultItinerary();
+				}
+			});
+			adultThread.setName("Adult " + i);
+			adultThread.fork();
+			set.add(adultThread);
+		}
 
+		for (int i = 0; i < children; i++) {
+			KThread childThread = new KThread(new Runnable() {
+				public void run() {
+					ChildItinerary();
+				}
+			});
+			childThread.setName("Child " + i);
+			childThread.fork();
+			set.add(childThread);
+		}
+
+		// wait for all threads to complete.
+		for (KThread t : set) {
+			t.join();
+		}
     }
 
     static void AdultItinerary()
     {
-	bg.initializeAdult(); //Required for autograder interface. Must be the first thing called.
-	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
+		bg.initializeAdult(); //Required for autograder interface. Must be the first thing called.
+		//DO NOT PUT ANYTHING ABOVE THIS LINE. 
 
-	/* This is where you should put your solutions. Make calls
-	   to the BoatGrader to show that it is synchronized. For
-	   example:
-	       bg.AdultRowToMolokai();
-	   indicates that an adult has rowed the boat across to Molokai
-	*/
+		// same code as childrenItinerary
+		lock.acquire();
+		adultsOnOahu++;
+		readyToLeaveOahu.wake();
+		readyToLeaveOahu.sleep();
+
+		while (boatLocation != 0 || boatIsInUse == boatFull || childrenOnOahu > 1) {
+			readyToLeaveOahu.sleep();
+		}
+		boatLock.acquire();
+		boatIsInUse = boatFull;
+		bg.AdultRowToMolokai();
+		adultsOnOahu--;
+		adultsOnMolokai++;
+		boatLocation = 1;
+		boatIsInUse = boatEmpty;
+		boatLock.release();
+
+		readyToLeaveMolokai.wake();
+		lock.release();
     }
 
     static void ChildItinerary()
     {
-	bg.initializeChild(); //Required for autograder interface. Must be the first thing called.
-	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
-    }
+		bg.initializeChild(); //Required for autograder interface. Must be the first thing called.
 
-    static void SampleItinerary()
-    {
-	// Please note that this isn't a valid solution (you can't fit
-	// all of them on the boat). Please also note that you may not
-	// have a single thread calculate a solution and then just play
-	// it back at the autograder -- you will be caught.
-	System.out.println("\n ***Everyone piles on the boat and goes to Molokai***");
-	bg.AdultRowToMolokai();
-	bg.ChildRideToMolokai();
-	bg.AdultRideToMolokai();
-	bg.ChildRideToMolokai();
+		// let everyone count first.
+		lock.acquire();
+		childrenOnOahu++;
+		readyToLeaveOahu.wake(); 	// wake up sleeping threads.
+		readyToLeaveOahu.sleep();
+
+		// child is ready to board on boat.
+		while (childrenOnOahu > 0) {
+			// wait for boat to come on Oahu
+			while (boatLocation != 0 || boatIsInUse == boatFull || childrenOnOahu == 1) {
+				readyToLeaveOahu.sleep();
+			}
+			/* Boat is on Oahu and leave now, but if there are more children,
+			 * carry them off too.
+			 */
+			boatLock.acquire();
+			if (boatIsInUse == boatEmpty) {
+				if (childrenOnOahu > 1) {
+					// more children that can board on this trip.
+					boatIsInUse = boatHalfFull;
+					readyToLeaveOahu.wake();
+					lock.release();
+					boatRiders.sleep();
+					lock.acquire();
+				} else {
+					// only one to row.
+					if (adultsOnOahu == 0) lastPersonLeft = true;
+					boatIsInUse = boatFull;
+				}
+				// ready to row the boat.
+				bg.ChildRowToMolokai();
+			} else if (boatIsInUse == boatHalfFull) {
+				boatIsInUse = boatFull;
+				if (childrenOnOahu == 2 && adultsOnOahu == 0) {
+					lastPersonLeft = true;
+				}
+				boatRiders.wake();
+				bg.ChildRideToMolokai();
+			}
+
+			childrenOnOahu--;
+			childrenOnMolokai++;
+			boatLocation = 1;
+			boatIsInUse = boatEmpty;
+			boatLock.release();
+
+			if (lastPersonLeft) {
+				readyToLeaveMolokai.wakeAll();
+				lock.release();
+				return;
+			}
+
+			readyToLeaveMolokai.wake();
+			readyToLeaveMolokai.sleep();
+
+			while (boatLocation != 1) {
+				readyToLeaveMolokai.sleep();
+			}
+
+			if (lastPersonLeft) {
+				lock.release();
+				return;
+			}
+
+			// ready to board to Oahu.
+			boatLock.acquire();
+			boatIsInUse = boatFull;
+			bg.ChildRowToOahu();
+			childrenOnOahu++;
+			childrenOnMolokai--;
+			boatLocation = 0;
+			boatIsInUse = boatEmpty;
+			boatLock.release();
+			lock.release();
+
+			//Reached Oahu.
+			lock.acquire();
+			readyToLeaveOahu.wakeAll();
+		}
     }
-    
 }
